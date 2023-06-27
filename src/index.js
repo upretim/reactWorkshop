12fb78f8bab83492de7733ce8e51d54688ba440f
upretim <manojkumar.upreti@live.com>
2023-06-27 20:58:06 +0530

    day 3 changes

--- src/index.js
+++ src/index.js
@@ -4,25 +4,55 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 //JSX
 
-let imageURL =
-  "https://m.media-amazon.com/images/I/41CNfYGhRvL._SX327_BO1,204,203,200_.jpg";
-let title = "Average Sucks";
-let author = "Michael Bernoff";
+let bookList = [
+  {
+    imageURL:
+      "https://m.media-amazon.com/images/I/41CNfYGhRvL._SX327_BO1,204,203,200_.jpg",
+    title: "Average Sucks",
+    author: "Michael Bernoff",
+    id: "1",
+  },
+  {
+    imageURL:
+      "https://m.media-amazon.com/images/I/41dmlKKCarL._SX327_BO1,204,203,200_.jpg",
+    title: "Lead or Get Out of the Way",
+    author: "Gary J Vien",
+    id: "2",
+  },
+  {
+    imageURL: "https://m.media-amazon.com/images/I/5165He67NEL.jpg",
+    title: "Lead or Get Out of the Way",
+    author: "Gary J Vien",
+    id: "3",
+  },
+];
 
-let imageURL2 =
-  "https://m.media-amazon.com/images/I/41dmlKKCarL._SX327_BO1,204,203,200_.jpg";
+let book1 = {
+  imageURL:
+    "https://m.media-amazon.com/images/I/41CNfYGhRvL._SX327_BO1,204,203,200_.jpg",
+  title: "Average Sucks",
+  author: "Michael Bernoff",
+};
 
-// let title2 = "Lead or Get Out of the Way";
-// let author2 = "Gary J Vien";
+let book2 = {
+  imageURL:
+    "https://m.media-amazon.com/images/I/41dmlKKCarL._SX327_BO1,204,203,200_.jpg",
+  title: "Lead or Get Out of the Way",
+  author: "Gary J Vien",
+};
 
-let imageURL3 = "https://m.media-amazon.com/images/I/5165He67NEL.jpg";
+let book3 = {
+  imageURL: "https://m.media-amazon.com/images/I/5165He67NEL.jpg",
+  title: "Lead or Get Out of the Way",
+  author: "Gary J Vien",
+};
 
 const BooKlist = () => {
   return (
     <section className="booklist">
-      <Book URL={imageURL} />
-      <Book URL={imageURL2} />
-      <Book URL={imageURL3} />
+      {bookList.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
     </section>
   );
 };
@@ -33,34 +63,34 @@ const AuthStyle = {
   marginTop: "0.5rem",
 };
 
-const Book = (props) => {
-  console.log("URL for image ", props.URL);
+const Book = ({ book }) => {
+  console.log("URL for image ");
   return (
     <article className="book">
-      <img src={props.URL} alt="Average Sucks" />
-      <h2>{title}</h2>
-      <h4 style={AuthStyle}>{author}</h4>
+      <img src={book?.imageURL} alt={book?.title} />
+      <h2>{book?.title}</h2>
+      <h4 style={AuthStyle}>{book?.author}</h4>
     </article>
   );
 };
 
-const Image = () => {
-  return <img src={imageURL} alt="Average Sucks" />;
-};
+// const Image = (props) => {
+//   return <img src={props.imageURL} alt="Average Sucks" />;
+// };
 
-const Title = () => {
-  return <h2>{title}</h2>;
-};
+// const Title = () => {
+//   return <h2>{title}</h2>;
+// };
 
-function Author() {
-  const AuthStyle = {
-    color: "#617d98",
-    fontSize: ".75rem",
-    marginTop: "0.5rem",
-  };
+// function Author() {
+//   const AuthStyle = {
+//     color: "#617d98",
+//     fontSize: ".75rem",
+//     marginTop: "0.5rem",
+//   };
 
-  return <h4 style={AuthStyle}>{author}</h4>;
-}
+//   return <h4 style={AuthStyle}>{author}</h4>;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BooKlist />);
